b45f8828e4933b2ab49266c03f5e8fcc0e989442
Riza Fahmi <rizafahmi@gmail.com>
2018-04-08 07:58:53 +0700

    Post data order

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 const Api = require("claudia-api-builder");
 const getPizzas = require("./handlers/get-pizzas");
+const createOrder = require("./handlers/create-order");
 
 const api = new Api();
 
@@ -8,5 +9,8 @@ api.get("/pizzas", () => getPizzas());
 api.get("/pizzas/{id}", req => getPizzas(req.pathParams.id), {
   error: 404
 });
-
+api.post("/orders", req => createOrder(req.body), {
+  success: 201,
+  error: 400
+});
 module.exports = api;
